6e826f37c347a2f08e99ac634b9e9e0021ff048e
asad <githubenter12@gmail.com>
2022-08-23 16:29:09 +0500

    nnn

---
+++ js.js
@@ -0,0 +1,8 @@
+var splide = new Splide( '.splide' );
+splide.mount();
+var splide2 = new Splide( '.ssss' );
+splide2.mount();
+var splide3 = new Splide( '.animate2' );
+splide3.mount();
+var splide4 = new Splide( '.name' );
+splide4.mount();
